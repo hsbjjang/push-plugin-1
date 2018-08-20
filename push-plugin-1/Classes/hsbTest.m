b17ddd9eeebf5fbd6bd6e6f55b9a60e093791d89
사이버이메지네이션 <ci@saibeoimejineisyeon-ui-MacBook-Pro.local>
2018-08-20 10:41:32 +0900

    Hah

---
+++ push-plugin-1/Classes/hsbTest.m
@@ -0,0 +1,16 @@
+//
+//  hsbTest.m
+//  push-plugin-1
+//
+//  Created by 사이버이메지네이션 on 2018. 8. 20..
+//
+
+#import <Foundation/Foundation.h>
+#import "hsbTest.h"
+
+@implementation hsb1
+
+-(void)HelloHsb {
+    NSLog(@"hsb hohohoho");
+}
+@end
